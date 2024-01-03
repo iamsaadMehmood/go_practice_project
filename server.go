2327542e4983eb550ff688ac7a11e9400a3c008e
iamsaadMehmood <warriorex.g@gmail.com>
2024-01-03 13:32:36 +0500

    update response using ResponseWriter

--- server.go
+++ server.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+type TestWebServerType bool
+
+func (m TestWebServerType) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello there !!!")
+}
 func main() {
-	http.ListenAndServe("localhost:8080", nil)
+	var k TestWebServerType
+	http.ListenAndServe("localhost:8080", k)
 }
